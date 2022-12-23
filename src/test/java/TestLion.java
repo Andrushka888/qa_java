71d6d2b229ac17e8ade316e8c4c5e4fe234cfe6f
Andrew Akimov <andrushka_akimov@mail.ru>
2022-12-23 22:21:36 +0300

    В класс TestLion добавлена обработка исключений через конструктор try и catch

--- src/test/java/TestLion.java
+++ src/test/java/TestLion.java
@@ -14,9 +14,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class TestLion {
-
     private final String lionGender;
-
     public TestLion(String lionGender) {
         this.lionGender = lionGender;
     }
@@ -33,7 +31,6 @@ public class TestLion {
     @Mock
     private Feline feline;
 
-
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -41,44 +38,41 @@ public class TestLion {
 
     @Test
     public void TestCountLionIsOne() throws Exception {
-
+    try {
         Lion lion = new Lion(lionGender,feline);
-
         Mockito.when(feline.getKittens()).thenReturn(1);
-
         int expectedLionCount = 1;
         int actualLionCount = lion.getKittens();
-
-        assertEquals(expectedLionCount, actualLionCount);
-
+         assertEquals(expectedLionCount, actualLionCount);
+    } catch(Exception exception) {
+            System.out.println("Должны использоваться только допустимые значения пола животного - Самец или Самка");
+       }
     }
-
 
     @Test
     public void TestLionHaveManeIfHeMale() throws Exception {
-
+        try {
         Lion lion = new Lion(lionGender, feline);
-
         boolean expectedPresenceMane = true;
         boolean actualPresenceMane = lion.doesHaveMane();
-
         assertEquals(expectedPresenceMane, actualPresenceMane);
-
+    } catch(Exception exception) {
+        System.out.println("Должны использоваться только допустимые значения пола животного - Самец или Самка");
+    } catch(AssertionError notExpected) {
+            System.out.println("Должен быть пол 'Самец'");
+        }
     }
-
 
     @Test
     public void TestLionEatingLikePredator() throws Exception {
-
+        try {
         Lion lion = new Lion(lionGender, feline);
-
         Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-
         List<String> expectedTypeEatingLion = List.of("Животные", "Птицы", "Рыба");
         List<String> actualTypeEatingLion = lion.getFood();
-
         assertEquals(expectedTypeEatingLion, actualTypeEatingLion);
-
+        } catch(Exception exception) {
+            System.out.println("Должны использоваться только допустимые значения пола животного - Самец или Самка");
+        }
     }
-
 }
